31a809754f9c5b54f0acec116e85e99ba2362d57
adib <adibfaizulhaq05@gmail.com>
2024-03-13 04:30:30 +0700

    menambahkan implementasi dengan table-driven

--- TP Modul 04/Program.cs
+++ TP Modul 04/Program.cs
@@ -8,6 +8,38 @@ namespace TP_Modul_04
 {
     internal class Program
     {
+        public class KodePos
+        {
+
+
+            public string getKodePos(Kelurahan kelurahan)
+            {
+                string[] kodePos = {"40266", "40287", "40267", "40256", "40287",
+                "40286", "40286", "40286", "40272", "40274",
+                "40273"
+                };
+                {
+                    int index = (int)kelurahan;
+                    return kodePos[index];
+                }
+            }
+        }
+
+        public enum Kelurahan
+        {
+            Batununggal,
+            Kujangsari,
+            Mengger,
+            Wates,
+            Cijaura,
+            Jatisari,
+            Margasari,
+            Sekejati,
+            Kebonwaru,
+            Maleer,
+            Samoja
+        }
+
         static void Main(string[] args)
         {
         }
